579d538417fc6bf26ce439327f3d0a5c34e84381
Rafael Deroncio <102925307+rderoncio@users.noreply.github.com>
2023-07-24 23:54:01 -0300

    Update servico-matricula.sql

--- scripts-ddl/servico-matricula.sql
+++ scripts-ddl/servico-matricula.sql
@@ -4,8 +4,6 @@ DROP TABLE IF EXISTS detalhe_contrato;
 CREATE TABLE detalhe_contrato (
   id SERIAL PRIMARY KEY,
   codigo_aluno VARCHAR(50) NOT NULL,
-  matricula VARCHAR(50) NOT NULL,
-  nome_curso VARCHAR(100) NOT NULL,
   codigo_curso VARCHAR(50) NOT NULL,
   codigo_forma_pagamento VARCHAR(50) NOT NULL,
   data_vencimento_pagamento DATE NOT NULL
