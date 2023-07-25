4a22de79fa9262d49a39d066f6ad8572125c6b56
Rafael Deroncio <102925307+rderoncio@users.noreply.github.com>
2023-07-25 20:33:15 -0300

    Update servico-inscricao.sql

--- scripts-ddl/servico-inscricao.sql
+++ scripts-ddl/servico-inscricao.sql
@@ -1,12 +1,16 @@
-DROP TABLE IF EXISTS candidato;
+DROP TABLE IF EXISTS inscricao_candidato;
 
-CREATE TABLE candidato (
+CREATE SEQUENCE codigo_inscricao_seq START 12345;
+
+CREATE TABLE inscricao_candidato (
   id SERIAL PRIMARY KEY,
+  codigo_inscricao INTEGER DEFAULT nextval('codigo_inscricao_seq') NOT NULL,
+  data_inscricao DATE NOT NULL,
   nome VARCHAR(100) NOT NULL,
   email VARCHAR(100) NOT NULL,
-  codigo_inscricao VARCHAR(20) NOT NULL,
-  curso_interesse VARCHAR(100),
-  data_inscricao DATE NOT NULL,
-  curso VARCHAR(100),
-  boleto VARCHAR(100)
+  telefone VARCHAR(20),
+  codigo_curso VARCHAR(50),
+  nome_curso VARCHAR(100),
+  boleto VARCHAR(100),
+  boleto_pago BOOLEAN
 );
